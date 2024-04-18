92c133c93a960e1f642f2f8e14c80e07969debe0
Minji Ardanuy-Jetté <minjiajette@gmail.com>
2024-04-18 11:23:24 -0400

    s10c2 Ajout d'une animation sur le carrousel et correction de la fermeture du carrousel

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,17 +1,18 @@
 (function () {
   console.log("vive Javascript !");
   let carrousel = document.querySelector(".carrousel");
-  console.log("conteneur carrousel : " + carrousel.tagNameme);
+  console.log("conteneur carrousel : " + carrousel.tagName);
   let bouton = document.querySelector(".bouton_ouvrir");
-  console.log("conteneur carrousel : " + bouton.tagNameme);
+  console.log("conteneur carrousel : " + bouton.tagName);
   let carrousel__x = document.querySelector(".carrousel__x");
-  console.log("conteneur carrousel : " + carrousel__x.tagNameme);
+  console.log("conteneur carrousel : " + carrousel__x.tagName);
 
+  /* Écouteur pour ouvrir la boîte modale */
   bouton.addEventListener("mousedown", function () {
     carrousel.classList.add("carrousel--ouvrir");
   });
-
+  /* Écouteur pour fermer la boîte modale */
   carrousel__x.addEventListener("mousedown", function () {
-    carrousel__x.classList.remove("carrousel--ouvrir");
+    carrousel.classList.remove("carrousel--ouvrir");
   });
 })();
